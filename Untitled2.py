db74f1e50590806f7532142a8043553eabeac8a1
faithtology1 <112449662+faithtology1@users.noreply.github.com>
2023-10-26 00:51:09 +0100

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,13 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("faith")
+print(name)
+color = input("red")
+animal = input("horse")
+print ("{}, you like a {} {}!".format(name,color,animal))
